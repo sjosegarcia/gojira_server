48e804e0b052d9ecc16539362d591c05267f77c7
Steven Garcia <sjosegarcia23@gmail.com>
2021-06-11 13:12:35 -0400

    user in db

--- services/web/endpoints/user/user_endpoints.py
+++ services/web/endpoints/user/user_endpoints.py
@@ -2,19 +2,21 @@ from fastapi.routing import APIRouter
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from repositories.database_repository import db
-from schema.user_schema import User
+from schema.user_schema import User, UserInDB
 from fastapi.exceptions import HTTPException
 from repositories.user_repository import create_user, get_user_by_email
 
 users_router = APIRouter()
 
 
-@users_router.put("/new-user", response_model=User)
-async def create_new_user(user: User, db: AsyncSession = Depends(db.get_db)) -> User:
+@users_router.put("/new-user", response_model=UserInDB)
+async def create_new_user(
+    user: User, db: AsyncSession = Depends(db.get_db)
+) -> UserInDB:
 
     user_found = await get_user_by_email(db, user.email)
     if user_found:
         raise HTTPException(status_code=400, detail="User already exist.")
 
     new_user = create_user(db, user)
-    return User.from_orm(new_user)
+    return UserInDB.from_orm(new_user)
