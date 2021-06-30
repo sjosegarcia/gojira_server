65c5bc2a5a7b952773fd12f0230dd73a5d48bd47
Steven Garcia <sjosegarcia23@gmail.com>
2021-06-30 12:24:28 -0400

    added some failed test cases

--- services/web/tests/unit/endpoints/user/test_user_endpoints.py
+++ services/web/tests/unit/endpoints/user/test_user_endpoints.py
@@ -20,12 +20,34 @@ async def test_create_new_user(
 
 
 @pytest.mark.asyncio
-async def test_get_user_id(test_client: AsyncClient, test_user: User) -> None:
+async def test_create_new_user_failed(
+    test_client: AsyncClient, test_user: User, mocker: MockerFixture
+) -> None:
+    mocker.patch("firebase_admin.auth.set_custom_user_claims")
     async with test_client as client:
-        response = await client.get(f"/user/id/1")
+        response = await client.put(
+            "/user/new",
+            json=test_user.dict(),
+        )
+    assert response.status_code == 400
+
+
+@pytest.mark.asyncio
+async def test_get_user_id(
+    test_client: AsyncClient, test_user: User, test_user_in_db: UserInDB
+) -> None:
+    async with test_client as client:
+        response = await client.get(f"/user/id/{test_user_in_db.id}")
     assert response.status_code == 200
     user_in_db = UserInDB(**response.json())
     assert user_in_db.uid == test_user.uid
+
+
+@pytest.mark.asyncio
+async def test_get_user_id_failed(test_client: AsyncClient, test_user: User) -> None:
+    async with test_client as client:
+        response = await client.get(f"/user/id/2")
+    assert response.status_code == 400
 
 
 @pytest.mark.asyncio
@@ -36,7 +58,7 @@ async def test_get_authenticated_user(
         response = await client.get(f"/user/me")
     assert response.status_code == 200
     user_in_db = UserInDB(**response.json())
-    # assert user_in_db.uid == test_user.uid
+    assert user_in_db.uid == test_user.uid
 
 
 @pytest.mark.asyncio
@@ -46,29 +68,3 @@ async def test_delete_current_user(test_client: AsyncClient, test_user: User) ->
     assert response.status_code == 200
     user_in_db = UserInDB(**response.json())
     assert user_in_db.email == test_user.email
-
-
-"""
-@pytest.mark.asyncio
-async def test_create_new_user_failed(
-    test_client: AsyncClient, test_user: User, mocker: MockerFixture
-) -> None:
-    mocker.patch("firebase_admin.auth.set_custom_user_claims")
-    failed_user = deepcopy(test_user.dict())
-    async with test_client as client:
-        response = await client.put(
-            "/user/new",
-            json=failed_user,
-        )
-    assert response.status_code == 400"""
-
-
-"""
-@pytest.mark.asyncio
-async def test_delete_current_user_failed(
-    test_client: AsyncClient, test_user: User
-) -> None:  # josh@aboutfresh.org jallen@mass-ventures.com
-    async with test_client as client:
-        response = await client.delete("/user/me/delete")
-    assert response.status_code == 405
-"""
