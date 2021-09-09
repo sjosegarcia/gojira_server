4c72e28e718456a6e566e754a122116d2f93afb6
Steven Garcia <sjosegarcia23@gmail.com>
2021-09-09 11:25:31 -0400

    startup

--- services/web/startup.sh
+++ services/web/startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-alembic upgrade head
+pipenv run alembic upgrade head
 if [ "$ENV" = "PRODUCTION" ]; then
 source .env.prod
-gunicorn setup.start_app:start_app -t 0 -w 1 -k uvicorn.workers.UvicornWorker -b $HOST:$PORT --threads 8 --log-level debug --forwarded-allow-ips="*"
+pipenv run gunicorn setup.start_app:start_app -t 0 -w 1 -k uvicorn.workers.UvicornWorker -b $HOST:$PORT --threads 8 --log-level debug --forwarded-allow-ips="*"
 else
 python -m debugpy --wait-for-client --listen $HOST:5678 -m uvicorn --factory setup.start_app:start_app --reload --host $HOST --port $PORT --lifespan on --log-level debug --forwarded-allow-ips '*' --workers 1
 fi
